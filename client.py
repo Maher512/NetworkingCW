5e6e5a9945eeddf75690c77f1e1ad2d0608129f2
Maher512 <mohamed.maher@tkh.edu.eg>
2021-12-21 00:57:52 +0200

    Working

--- client.py
+++ client.py
@@ -1,17 +1,17 @@
 import socket
 
 # Create a client socket
-cSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+cSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect to the server
-cSocket.connect(("127.0.0.1",25));
+cSocket.connect(("127.0.0.1",25))
 
 # Send data to server
-message = input("Please enter your message:");
-cSocket.send(message.encode());
+message = input("Please enter your message:")
+cSocket.send(message.encode())
 
 # Receive data from server
-server_data = cSocket.recv(1024);
+server_data = cSocket.recv(1024)
 
 # Print to the console
-print(server_data.decode());
+print(server_data.decode())
--- server.py
+++ server.py
@@ -1,19 +1,19 @@
 import socket
 
 # operating on IPv4 addressing scheme
-sSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+sSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # This is to bind and listen to the server
-sSocket.bind(("127.0.0.1",25));
-sSocket.listen();
+sSocket.bind(("127.0.0.1",25))
+sSocket.listen()
 
 # Accept connections
 while(True):
-    (cConnected, cAddress) = sSocket.accept();
-    print("Accepted a connection request from %s:%s"%(cAddress[0], cAddress[1]));
+    (cConnected, cAddress) = sSocket.accept()
+    print("Accepted a connection request from %s:%s"%(cAddress[0], cAddress[1]))
     client_data = cConnected.recv(1024)
-    print(client_data.decode());
+    print(client_data.decode())
 
     # Send the data back to the client
-    cConnected.send("Hello Client:)".encode());
+    cConnected.send("Hello Client:)".encode())
 
