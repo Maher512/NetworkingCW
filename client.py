14cc3cf0918d05947594087bd4f3ce08e06c8300
Maher512 <mohamed.maher@tkh.edu.eg>
2021-12-21 00:54:11 +0200

    Working

--- client.py
+++ client.py
@@ -1,1 +1,17 @@
-print("Client")
+import socket
+
+# Create a client socket
+cSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+
+# Connect to the server
+cSocket.connect(("127.0.0.1",25));
+
+# Send data to server
+message = input("Please enter your message:");
+cSocket.send(message.encode());
+
+# Receive data from server
+server_data = cSocket.recv(1024);
+
+# Print to the console
+print(server_data.decode());
